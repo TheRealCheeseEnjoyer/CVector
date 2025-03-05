3a870a3404f70a853b554e58267d482143ba5e52
TheRealCheeseEnjoyer <57392663+TheRealCheeseEnjoyer@users.noreply.github.com>
2025-03-05 03:14:06 +0100

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -4,18 +4,18 @@
 
 typedef struct vector_t* Vector;
 
-Vector Vector_Init(size_t typeSize);
-Vector Vector_InitC(size_t typeSize, size_t capacity);
+Vector Vec_Init(size_t typeSize);
+Vector Vec_InitC(size_t typeSize, size_t capacity);
 
-void Vector_Resize(Vector v);
-void* Vector_Append(Vector v, void* item);
-void* Vector_Get(Vector v, size_t index);
-void Vector_Remove(Vector v, size_t index);
-void Vector_Clear(Vector v);
+void Vec_Resize(Vector v);
+void* Vec_Append(Vector v, void* item);
+void* Vec_Get(Vector v, size_t index);
+void Vec_Remove(Vector v, size_t index);
+void Vec_Clear(Vector v);
 
-size_t Vector_Size(Vector v);
-size_t Vector_Capacity(Vector v);
+size_t Vec_Size(Vector v);
+size_t Vec_Capacity(Vector v);
 
-void Vector_Free(Vector v);
+void Vec_Free(Vector v);
 
 #endif
